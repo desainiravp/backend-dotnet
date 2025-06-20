0068950e76797809bf150dd7b99cc9861b998c12
Nirav Desai <desai.nirav.p@gmail.com>
2025-06-20 15:57:22 +0530

    Add new file

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Hello from .NET Backend!");
+}
