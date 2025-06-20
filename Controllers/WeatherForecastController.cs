bee5089effa960674d77e1adedc075407bbbabeb
nirav843 <nirav.p.desai@slscorp.com>
2025-06-20 16:56:36 +0530

    source removed

--- Controllers/WeatherForecastController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
-{
-    [HttpGet]
-    public IActionResult Get() => Ok("Hello from .NET Backend!");
-}
